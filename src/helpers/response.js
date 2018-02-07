30f1241542d937641ed2bc4c499ba6a4d9ec7d90
Sidmar Theodoro <theodorosidmar@gmail.com>
2018-02-06 23:54:30 -0200

    [FEAT] Added helper for default responses

---
+++ src/helpers/response.js
@@ -0,0 +1,10 @@
+const httpStatus = require('http-status');
+
+exports.defaultResponse = (data, statusCode = httpStatus.OK) => ({
+  data,
+  statusCode,
+});
+
+exports.errorResponse = (message, statusCode = httpStatus.BAD_REQUEST) => exports.defaultResponse({
+  error: { message },
+}, statusCode);
